56eb237c62d6eb82efd15884d5a0ac6997e2c8f1
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-24 00:13:53 +0200

    [add]: feat: 🚧 postgres integer id fix

--- src/main/java/com/example/transactionappv2/repositories/ParentTransactionRepository.java
+++ src/main/java/com/example/transactionappv2/repositories/ParentTransactionRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface ParentTransactionRepository extends JpaRepository<ParentTransaction, UUID> {
+public interface ParentTransactionRepository extends JpaRepository<ParentTransaction, Integer> {
 
 }
