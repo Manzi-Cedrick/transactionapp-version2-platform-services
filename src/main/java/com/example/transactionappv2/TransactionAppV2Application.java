eb84804258c9685251f5c726a9714894f8dac73e
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-23 16:53:40 +0200

    [add]: feat: 🚧 project initial spring configs

---
+++ src/main/java/com/example/transactionappv2/TransactionAppV2Application.java
@@ -0,0 +1,13 @@
+package com.example.transactionappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionAppV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionAppV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/transactionappv2/TransactionAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transactionappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionAppV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
