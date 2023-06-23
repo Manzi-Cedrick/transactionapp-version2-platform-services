552ff85c8b3cac47946cf77929ea1e009080d2aa
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-23 23:20:49 +0200

    [add]: feat: 🚧 parent transaction model addition

--- src/main/java/com/example/transactionappv2/models/ParentTransaction.java
+++ src/main/java/com/example/transactionappv2/models/ParentTransaction.java
@@ -1,9 +1,13 @@
 package com.example.transactionappv2.models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-import java.security.PublicKey;
-
+@ToString
+@Getter
+@Setter
 @Entity
 public class ParentTransaction {
     @Id
