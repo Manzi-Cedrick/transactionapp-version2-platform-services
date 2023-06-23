89e3e5eaa405ebfd9d203a530bfc1b37380872c9
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-24 00:13:12 +0200

    [add]: feat: 🚧 dto initialization

---
+++ src/main/java/com/example/transactionappv2/dto/ParentTransactionDTO.java
@@ -0,0 +1,8 @@
+package com.example.transactionappv2.dto;
+
+public class ParentTransactionDTO {
+    private Integer id;
+    public String sender;
+    public String receiver;
+    public Number totalAmount;
+}
