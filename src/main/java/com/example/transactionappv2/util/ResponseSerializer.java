48769752a70a73c0afbb9df3ffbce6bc44ec5e00
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-23 23:22:04 +0200

    [add]: feat: 🚧 initial response format

---
+++ src/main/java/com/example/transactionappv2/util/ResponseSerializer.java
@@ -0,0 +1,12 @@
+package com.example.transactionappv2.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ResponseSerializer {
+    private String message;
+}
