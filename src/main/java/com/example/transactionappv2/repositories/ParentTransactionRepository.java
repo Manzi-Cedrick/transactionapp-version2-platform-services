e6e7448cb4dab46d0d974e05c5d0217f36ecb4b8
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-24 02:05:59 +0200

    [update]: feat: 🚧 clean unnecessary packages

--- src/main/java/com/example/transactionappv2/repositories/ParentTransactionRepository.java
+++ src/main/java/com/example/transactionappv2/repositories/ParentTransactionRepository.java
@@ -3,7 +3,6 @@ package com.example.transactionappv2.repositories;
 import com.example.transactionappv2.models.ParentTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
 
 public interface ParentTransactionRepository extends JpaRepository<ParentTransaction, Integer> {
 
